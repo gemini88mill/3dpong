88363524ec9c045dafc301bfdf1d2705f5705d95
Raphael <gemini88mill@gmail.com>
2017-09-03 19:24:17 -0400

    fixed paddles

--- js/movement.js
+++ js/movement.js
@@ -9,44 +9,59 @@ function ballMovement(){
     ball.position.x += xDir;
     ball.position.y += yDir;
 
-    if(ball.position.x < -25){
+    if(ball.position.x < -24){
         xDir = 0.2;
+
+        if(Math.abs(playerPaddle.position.y - ball.position.y) <=5){
+            yDir = -yDir + 0.5;
+        } else{
+            playerScore += playerScore;
+            ball.position.x = ball.position.y = 0;
+        }
+
     }
-    else if( ball.position.x > 25 )
+    else if( ball.position.x > 24 )
     {
         xDir = -0.2;
         //four.play();
+
+        if(Math.abs(cpuPaddle.position.y - ball.position.y) <=5){
+            yDir = -yDir + 0.5;
+        } else{
+            playerScore += playerScore;
+            ball.position.x = ball.position.y = 0;
+        }
     }
 
-    if( ball.position.y < -15 && yDir < 0 )
+    if( ball.position.y < -14 && yDir < 0 )
     {
         yDir = 0.4;
 
-        if( Math.abs( paddle2.position.x - ball.position.x ) <= 2 )
-        {
-            xDir = -xDir;
-            //one.play();
-        }
-        else
-        {
-            ball.position.x = ball.position.y = 0;
-            //explode.play();
-        }
+        // if( Math.abs( paddle2.position.x - ball.position.x ) <= 2 )
+        // {
+        //     xDir = -xDir;
+        //     //one.play();
+        // }
+        // else
+        // {
+        //     ball.position.x = ball.position.y = 0;
+        //     //explode.play();
+        // }
     }
-    else if( ball.position.y > 15 && yDir > 0 )
+    else if( ball.position.y > 14 && yDir > 0 )
     {
         yDir = -0.4;
 
-        if( Math.abs( paddle1.position.x - ball.position.x ) <= 2 )
-        {
-            xDir = -xDir;
-            two.play();
-        }
-        else
-        {
-            ball.position.x = ball.position.y = 0;
-            explode.play();
-        }
+        // if( Math.abs( paddle1.position.x - ball.position.x ) <= 2 )
+        // {
+        //     xDir = -xDir;
+        //     two.play();
+        // }
+        // else
+        // {
+        //     ball.position.x = ball.position.y = 0;
+        //     explode.play();
+        // }
     }
 }
 
@@ -57,7 +72,7 @@ function userPaddleMovement(){
 }
 
 function cpuPaddleMovement(){
-    cpuPaddle.position.y += yDir;
+    cpuPaddle.position.y = ball.position.y;
 }
 
 var mx = 0, my = 0;
